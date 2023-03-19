8e48abcfc5e7d8567b8c301790f04ade5c9b936a
Sergey Belov <sergeytrenerbelov@gmail.com>
2023-03-19 13:26:17 +0200

    3243214

--- api/index.js
+++ api/index.js
@@ -45,7 +45,7 @@ app.use(flash());
 app.use(varMiddleware);
 app.use(userMidlleware);
 
-app.use("/api", homeRoutes);
+app.use("/", homeRoutes);
 app.use("/add", addRoutes);
 app.use("/courses", coursesRoutes);
 app.use("/card", cardRoutes);
