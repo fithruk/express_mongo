1bf861c8716d6f9b8a161896012b9c86a771ba53
Sergey Belov <sergeytrenerbelov@gmail.com>
2023-03-19 14:43:15 +0200

    q3241

--- keys/keys.prod.js
+++ keys/keys.prod.js
@@ -1,6 +1,6 @@
 module.exports = {
   MONGODB_URI: process.env.MONGODB_URI,
   SESSION_SECRET: process.env.SESSION_SECRET,
-  // BASE_URL: process.env.BASE_URL,
+  BASE_URL: process.env.BASE_URL,
   SENDGREED_API_KEY: process.env.SENDGREED_API_KEY,
 };
