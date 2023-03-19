5c837056c2809b5cb3b958e6ac43485500ac273b
fithruk <100044405+fithruk@users.noreply.github.com>
2023-03-19 19:42:17 +0200

    Update keys.dev.js

--- keys/keys.dev.js
+++ keys/keys.dev.js
@@ -1,7 +1,3 @@
 module.exports = {
-  MONGODB_URI: `mongodb+srv://deathrow:deathrow@cluster0.wwrcc1f.mongodb.net/shop`,
-  SESSION_SECRET: "some secret",
-  BASE_URL: "http://localhost:5000",
-  SENDGREED_API_KEY:
-    "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",
+  
 };
