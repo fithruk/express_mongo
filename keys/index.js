ad2bbefd06232d1ed90b29be6c937c1419e7a427
Sergey Belov <sergeytrenerbelov@gmail.com>
2023-03-19 23:05:45 +0200

    124323

--- keys/index.js
+++ keys/index.js
@@ -1,6 +1,3 @@
 if (process.env.NODE_ENV === "production") {
   module.exports = require("./keys.prod");
-} else {
-  module.exports = require("./keys.dev");
 }
-module.exports = require("./keys.dev");
