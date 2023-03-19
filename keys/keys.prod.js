9b68fd2d7ff9e3794b5b6d3671ce6734231f855a
Sergey Belov <sergeytrenerbelov@gmail.com>
2023-03-19 12:18:59 +0200

    1212

--- keys/keys.prod.js
+++ keys/keys.prod.js
@@ -1,6 +1,6 @@
 module.exports = {
   MONGODB_URI: process.env.MONGODB_URI,
   SESSION_SECRET: process.env.SESSION_SECRET,
-  BASE_URL: "express-mongo-shop-lsh8dq08b-fithruk.vercel.app",
+  BASE_URL: process.env.BASE_URL,
   SENDGREED_API_KEY: process.env.SENDGREED_API_KEY,
 };
